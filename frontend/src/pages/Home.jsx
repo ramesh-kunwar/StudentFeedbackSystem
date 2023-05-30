3dd127f11380d5add7555f19fc1a2303ef4a81c8
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-05-31 00:46:28 +0545

    Home page UI design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,20 +4,28 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-page">
-      <div className="home-image">
-        <img src={homeImage} alt="" />
-      </div>
-      <div className="home-content">
-        <h1>
-          Let's Make Education <br /> More Effective
-        </h1>
-
-        <a href="#">
-          <button className="btn btn-border">View Teachers</button>
-        </a>
-        <a href="#">
-          <button className="btn btn-color">View Semesters</button>
-        </a>
+      <div className="home-page-wrapper">
+        <div className="item item-left">
+          <h1>
+            Lets Make <span>Education</span> More Effective
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo,
+            recusandae labore. Non incidunt ab quas beatae consectetur hic
+            quaerat natus.
+          </p>
+          <div className="buttons">
+            <a href="" className="btn btn-teachers">
+              View Teachers
+            </a>
+            <a href="" className="btn btn-semester">
+              View Semester
+            </a>
+          </div>
+        </div>
+        <div className="item item-right">
+          <img src={homeImage} alt="HOme image" />
+        </div>
       </div>
     </div>
   );
