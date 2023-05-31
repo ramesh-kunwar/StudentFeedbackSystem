0ef3d490baa2947d688b1665efb759cfeb77e984
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-05-31 18:32:44 +0545

    Navbar Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Login from './pages/Login'
-import Home from './pages/Home'
+import React from "react";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="">
-
+      <Navbar />
       {/* <Login /> */}
-      <Home />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
 // import 'bootstrap/dist/css/bootstrap.css'
-import './index.css'
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route  path="/login" element={<Login />} />
+      {/* <Route path="/product/:id" element={<ProductScreen />} /> */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
