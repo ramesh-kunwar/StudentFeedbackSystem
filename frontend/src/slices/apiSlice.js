4122d6fe732000d116a3bf5641b68a8f3cdc0be0
ramesh-kunwar <rameshkunwar1110@gmail.com>
2026-06-16 22:42:02 +0545

    Fix 401 on protected routes by attaching Bearer token to all API requests

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,16 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth?.userInfo?.token;
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
